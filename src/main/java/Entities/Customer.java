3c9823598cd7a3ee6700809f7cdba80849891296
Malthorn1 <mikkelskott@gmail.com>
2020-02-26 17:54:27 +0100

    done

--- src/main/java/Entities/Customer.java
+++ src/main/java/Entities/Customer.java
@@ -16,6 +16,7 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
+import javax.persistence.OneToOne;
 
 /**
  *
@@ -30,6 +31,9 @@ public class Customer implements Serializable {
     private Long id;
     private String FirstName; 
     private String LastName; 
+    @OneToOne
+    private Adress adress; 
+    
     
     
     
